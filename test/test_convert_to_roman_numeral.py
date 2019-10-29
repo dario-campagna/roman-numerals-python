90a9340e4e14c4936cfe2bbf4a7c10b5dadc1bfc
Dario Campagna <campagna@esteco.com>
2019-10-29 11:47:03 +0100

    We now convert 18 to XVIII
    Signed-off-by: Dario Campagna <campagna@esteco.com>

--- test/test_convert_to_roman_numeral.py
+++ test/test_convert_to_roman_numeral.py
@@ -14,21 +14,24 @@ class TestConvert(unittest.TestCase):
 
     def test_3_is_III(self):
         self.assertEqual('III', str(RomanNumeral(3)))
-    
+
     def test_4_is_IV(self):
         self.assertEqual('IV', str(RomanNumeral(4)))
-    
+
     def test_5_is_V(self):
         self.assertEqual('V', str(RomanNumeral(5)))
-    
+
     def test_6_is_VI(self):
         self.assertEqual('VI', str(RomanNumeral(6)))
-    
+
     def test_7_is_VII(self):
         self.assertEqual('VII', str(RomanNumeral(7)))
-    
+
     def test_9_is_IX(self):
         self.assertEqual('IX', str(RomanNumeral(9)))
-    
+
     def test_10_is_X(self):
         self.assertEqual('X', str(RomanNumeral(10)))
+
+    def test_18_is_XVIII(self):
+        self.assertEqual('XVIII', str(RomanNumeral(18)))
